41963b7e70be0beab40ac2d12b80a8f604da956b
SteisAndreeva <steis_ka@abv.bg>
2017-04-07 19:50:09 +0300

    BlogSoftuniJS

---
+++ SoftUniBlog/models/User.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+const encryption = require('./../utilities/encryption');
+const Role = mongoose.model('Role');
+
+let userSchema = mongoose.Schema(
+    {
+        email: {type: String, required: true, unique: true},
+        passwordHash: {type: String, required: true},
+        fullName: {type: String, required: true},
+        articles: [{type: mongoose.Schema.Types.ObjectId, ref:'Article' }],
+        roles: [{type: mongoose.Schema.Types.ObjectId, ref:'Role' }],
+        salt: {type: String, required: true}
+    }
+);
+
+userSchema.method ({
+   authenticate: function (password) {
+       let inputPasswordHash = encryption.hashPassword(password, this.salt);
+       let isSamePasswordHash = inputPasswordHash === this.passwordHash;
+
+       return isSamePasswordHash;
+   }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
+
+module.exports.seedAdmin =()=>{
+    let email = 'admin@softuni.bg';
+    User.findOne({email: email}).then(admin=>{
+        if(!admin){
+            Role.findOne({name: 'Admin'}).then(role =>{
+                if(!role){
+                    return;
+                }
+                let salt = encryption.generateSalt();
+                let passwordHash = encryption.hashPassword('admin', salt);
+
+                let roles=[];
+                roles.push(role.id);
+
+                let user ={
+                    email: email,
+                    passwordHash: passwordHash,
+                    fullName: 'Admin',
+                    articles: [],
+                    salt: salt,
+                    roles: roles
+                };
+
+                User.create(user).then(user =>{
+                    role.user.push(user.id);
+                    role.save(err =>{
+                        if(err) {
+                            console.log(err.message);
+                        }else {
+                            console.log('Admin seeded succeesfully! ');
+                        }
+                    });
+                });
+            });
+        }
+    });
+};
+
+
